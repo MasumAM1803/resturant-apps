021ae62f67941cb6537c87d7eb1b4ceac60961b7
MasumAM1803 <masum.abdulm@gmail.com>
2024-01-29 18:49:43 +0700

    clean code configuration drawer

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,7 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/scss/main.scss';
 import './components/index';
+import App from './views/app';
 
 // Import javascript file as needed
 import Home from './pages/home';
@@ -23,22 +24,29 @@ const initPages = () => {
   }
 };
 
+
 window.addEventListener('DOMContentLoaded', async () => {
-  const menu = document.querySelector('#menu');
-  const main = document.querySelector('main');
-  const drawer = document.querySelector('#drawer');
+  // const menu = document.querySelector('#menu');
+  // const main = document.querySelector('main');
+  // const drawer = document.querySelector('#drawer');
 
   initPages();
 
   const route = detectRoute();
   route.init();
 
-  menu.addEventListener('click', function (event) {
-    drawer.classList.toggle('open');
-    event.stopPropagation();
+  const app = new App({
+    button: document.querySelector('#menu'),
+    drawer: document.querySelector('#drawer'),
+    content: document.querySelector('#main'),
   });
   
-  main.addEventListener('click', function () {
-    drawer.classList.remove('open');
-  });
+  // menu.addEventListener('click', function (event) {
+  //   drawer.classList.toggle('open');
+  //   event.stopPropagation();
+  // });
+  
+  // main.addEventListener('click', function () {
+  //   drawer.classList.remove('open');
+  // });
 });
