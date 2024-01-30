8f2b4accaacdafecd791967e35a852328a9570b5
MasumAM1803 <masum.abdulm@gmail.com>
2024-01-31 00:13:58 +0700

    delete unused code

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,7 +4,6 @@ import './components/index';
 import App from './views/app';
 import swRegister from './utils/sw-register';
 // import WebSocketInitiator from './utils/websocket-initiator';
-// import FooterToolsInitiator from './utils/footer-tools-initiator';
 // import CONFIG from './globals/config';
 
 window.addEventListener('DOMContentLoaded', async () => {
@@ -22,9 +21,5 @@ window.addEventListener('DOMContentLoaded', async () => {
     app.renderPage();
     swRegister();
     // WebSocketInitiator.init(CONFIG.WEB_SOCKET_SERVER);
-    // FooterToolsInitiator.init({
-    //   subscribeButton: document.querySelector('#subscribePushNotification'),
-    //   unsubscribeButton: document.querySelector('#unsubscribePushNotification'),
-    // });
   });
 });
